e8bc23aa44aa625a71d09d2f5974171cb770dbc6
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-09-25 20:58:44 -0400

    Initialisation projet

---
+++ src/projet-cpp-2018-VinSey1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : projet-cpp-2018-VinSey1.cpp
+// Author      : Vincent
+// Version     :
+// Copyright   : @vseyller
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "test" << endl; // prints test
+	return 0;
+}
