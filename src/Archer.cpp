39249420840c11d0ddda411ea73639e9a371b053
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-09-26 11:18:52 -0400

    Création classe Archer

---
+++ src/Archer.cpp
@@ -0,0 +1,21 @@
+/*
+ * Archer.cpp
+ *
+ *  Created on: 26 sept. 2018
+ *      Author: Vincent
+ */
+
+#include "Archer.h"
+
+namespace DonjonEtDragons {
+
+Archer::Archer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Archer::~Archer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace DonjonEtDragons */
---
+++ src/Archer.h
@@ -0,0 +1,21 @@
+/*
+ * Archer.h
+ *
+ *  Created on: 26 sept. 2018
+ *      Author: Vincent
+ */
+
+#ifndef ARCHER_H_
+#define ARCHER_H_
+
+namespace DonjonEtDragons {
+
+class Archer {
+public:
+	Archer();
+	virtual ~Archer();
+};
+
+} /* namespace DonjonEtDragons */
+
+#endif /* ARCHER_H_ */
