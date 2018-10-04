8f148abe90450a2fca7503a37ab1e458055f55bb
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-10-03 20:38:04 -0400

    Ajout Arme Constructeur Archer
    Á sa construction, l'archer obtient un arc de puissance 75

--- src/Archer.cpp
+++ src/Archer.cpp
@@ -6,31 +6,38 @@
  */
 
 #include "Archer.h"
+#include "Arme.h"
 #include <sstream>
 
 namespace DonjonEtDragons {
 
 Archer::Archer() : Joueur() {
 	this->agilite = 0;
+	this->arme = new Arme("Arc", 75);
 }
 
 Archer::Archer(string nom) : Joueur(nom) {
 	this->nom = nom;
 	this->agilite = 0;
+	this->arme = new Arme("Arc", 75);
 }
 
 Archer::Archer(string nom, int agilite) : Joueur(nom) {
 	this->nom = nom;
 	this->agilite = agilite;
+	this->arme = new Arme("Arc", 75);
 }
 
 Archer::Archer(string nom, int armure, int agilite) : Joueur(nom, armure) {
 	this->nom = nom;
 	this->armure = armure;
 	this->agilite = agilite;
+	this->arme = new Arme("Arc", 75);
 }
 
-Archer::~Archer() {}
+Archer::~Archer() {
+	delete this->arme;
+}
 
 string Archer::exporter() {
 	stringstream xml;
