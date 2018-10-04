a8d63655b0c9fd0f228d6210ab09248887738559
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-10-03 20:39:15 -0400

    Ajout Arme Constructeur Voleur
    Á sa construction, le voleur obtient une dague de puissance 50

--- src/Voleur.cpp
+++ src/Voleur.cpp
@@ -6,31 +6,38 @@
  */
 
 #include "Voleur.h"
+#include "Arme.h"
 #include <sstream>
 
 namespace DonjonEtDragons {
 
 Voleur::Voleur() : Joueur() {
 	this->finesse = 0;
+	this->arme = new Arme("Dague", 50);
 }
 
 Voleur::Voleur(string nom) : Joueur(nom) {
 	this->nom = nom;
 	this->finesse = 0;
+	this->arme = new Arme("Dague", 50);
 }
 
 Voleur::Voleur(string nom, int finesse) : Joueur(nom) {
 	this->nom = nom;
 	this->finesse = finesse;
+	this->arme = new Arme("Dague", 50);
 }
 
 Voleur::Voleur(string nom, int armure, int finesse) : Joueur(nom, armure) {
 	this->nom = nom;
 	this->armure = armure;
 	this->finesse = finesse;
+	this->arme = new Arme("Dague", 50);
 }
 
-Voleur::~Voleur() {}
+Voleur::~Voleur() {
+	delete this->arme;
+}
 
 string Voleur::exporter() {
 	stringstream xml;
